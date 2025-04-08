f927ee4d764d4c56f2e638c0de1ce7439847d113
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2025-04-08 08:44:07 +0100

    Seeded db

--- src/app/db/seed.ts
+++ src/app/db/seed.ts
@@ -1,13 +1,57 @@
-// import { PrismaClient } from '@prisma/client';
-// import { recipes as sampleData } from './sample-data';
+import { PrismaClient } from "@prisma/client";
+import { recipes as sampleData } from "./sample-data";
 
-// async function main() {
-//   const prisma = new PrismaClient();
-//   await prisma.recipe.deleteMany();
+async function main() {
+  const prisma = new PrismaClient();
+  await prisma.ingredient.deleteMany({});
+  await prisma.nutritionalValue.deleteMany({});
+  await prisma.source.deleteMany({});
+  await prisma.recipe.deleteMany({});
 
-//   await prisma.recipe.createMany({ data: sampleData });
+  //   await prisma.recipe.createMany({ data: sampleData });
+  for (const recipeData of sampleData) {
+    const { ingredients, nutritionalValue, source, ...recipeFields } =
+      recipeData;
 
-//   console.log('Database seeded successfully');
-// }
+    const recipe = await prisma.recipe.create({
+      data: {
+        ...recipeFields,
+      },
+    });
 
-// main();
+    if (ingredients) {
+      for (const ingredient of ingredients) {
+        await prisma.ingredient.create({
+          data: {
+            ...ingredient,
+            recipeId: recipe.id,
+          },
+        });
+      }
+    }
+
+    // Create nutritional value
+    if (nutritionalValue) {
+      await prisma.nutritionalValue.create({
+        data: {
+          ...nutritionalValue,
+          recipeId: recipe.id,
+        },
+      });
+    }
+
+    // Create source
+    if (source) {
+      await prisma.source.create({
+        data: {
+          ...source,
+          recipeId: recipe.id,
+        },
+      });
+    }
+  }
+
+  console.log("Database seeded successfully");
+}
+
+main();
